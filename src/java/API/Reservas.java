febfd3cae6951975272b8de40a6835bb5da15977
StivenSanchezRico <ricko2013em@gmail.com>
2018-10-18 23:36:29 -0500

    Gran parte de las reservas

--- src/java/API/Reservas.java
+++ src/java/API/Reservas.java
@@ -5,9 +5,13 @@
  */
 package API;
 
+import java.io.*;
+import java.io.FileWriter;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
@@ -15,13 +19,47 @@ import javax.ws.rs.core.MediaType;
  *
  * @author W
  */
-   
+@Path("Reservas")
 public class Reservas {
    @POST
-   @Path("/create")
-   @Consumes(MediaType.APPLICATION_JSON)
-    public String reservas(){
-        return "HOLA";
+   @Path("crear")
+   @Consumes("application/json")
+   @Produces("application/json")
+   public String agregarReserva(String Archivo) {
+     Escribir("Reserva.txt",Archivo);
+     return Archivo;
+   }
+   public static void Escribir(String nombre, String Reserva)
+    {
+        File archivo;
+        FileWriter escribir;
+        PrintWriter linea;
+        
+        try {
+            archivo = new File(nombre);
+            if(!archivo.exists()){
+                archivo.createNewFile();
+                escribir = new FileWriter(archivo,true);
+                linea = new PrintWriter(escribir);
+                //Se escribe en el archivo
+                linea.println(Reserva);
+                linea.close();
+                escribir.close();
+            }else{
+                escribir = new FileWriter(archivo,true);
+                linea = new PrintWriter(escribir);
+                //Se escribe en el archivo
+                linea.println(Reserva);
+                linea.close();
+                escribir.close();
+            }
+            
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
+   
     
+   
 }
--- web/V1/Reservas.jsp
+++ web/V1/Reservas.jsp
@@ -9,12 +9,30 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <script>
+             
+
+            function enviarRequest() {
+                // obtenemos e valor por el numero de elemento
+                var tipoReserv =document.getElementsByName("tipoReserva")[0].value;
+                var cantid = document.getElementsByName("cantidad")[0].value;
+                window.alert(tipoReserv + cantidad);
+                var json={datos:[{tipoReserva: tipoReserv},{cantidad:cantid}]};
+                var obj = JSON.parse(json);
+                location.href ="/ServiciosEnciclaAPI/app/Reservas/crear";
+            }
+        </script>
         <title>JSP Page</title>
     </head>
     <body>
         <h1>BIENVENIDO AL SERVICIO DE RESERVAS DE ENCICLA!</h1>
         <form name="miformulario" action="mipagina.php" method="POST">
-        <input type="text" size="20" name="mivariable1">
+        <h5>Digíta el tipo de reserva que se realizara</h5>
+        <input type="text" size="20" name="tipoReserva">
+        <h5>Digíta la cantidad de personas a la que se le realizara la reserva</h5>
+        <input type="text" size="20" name="cantidad">
+        <input type="button" value="Realizar Reserva" onclick="enviarRequest()"/></br>
         </form>
     </body>
 </html>
